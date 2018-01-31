7fc4d2af466caa53c3fe4a4f9d32dcd22a8e6913
L-wiedii <lwiedii@feliperoberto.com.br>
2018-01-31 20:06:09 -0200

    First attempt

---
+++ plot2.R
@@ -0,0 +1,21 @@
+setwd('~/Documentos/coursera/ExData_Plotting1/')
+if(!file.exists('household_power_consumption.zip')){
+  download.file(url='https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip', destfile = 'household_power_consumption.zip', method = 'curl')
+}
+fulldata<-read.table(unz('household_power_consumption.zip',"household_power_consumption.txt"),header = T,sep = ";",na.strings = c("?"))
+fulldata$DateTime<-strptime(paste(fulldata$Date,fulldata$Time,sep = ' '),"%d/%m/%Y %H:%M:%S")
+#fulldata$Date<-strptime(fulldata$Date,"%d/%m/%Y")
+data<-subset(fulldata,
+  DateTime >= '2007-02-01 00:00:00' & 
+  DateTime <= '2007-02-02 23:59:59'
+)
+png(file="plot2.png", width=504, height=504)
+par(mfrow=c(1,1))
+plot(
+  data$DateTime,
+  data$Global_active_power,
+     type = "l",
+     ylab = 'Global Active Power (kilowatts)',
+     xlab = NA
+)
+dev.off()
